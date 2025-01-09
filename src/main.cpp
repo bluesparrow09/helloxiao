826ef0556571f9e5ad5a3f5407b512134f2d24eb
Chunzhi <chunzhi@uw.com>
2025-01-08 17:20:56 -0800

    second commmit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+  /*
+    hello_xiao.ino
+
+    A first sketch for the Seeeduino XIAO in PlatformIO
+  
+    This example code is in the public domain.
+  
+    Michel Deslierres  
+    June 15, 2020 
+  */
+  
+  #include <Arduino.h>  // needed in PlatformIO
+  
+  void setup() {
+  }
+  
+  void loop() {
+    Serial.println("Hello XIAO!");
+    delay(2000);
+   }
